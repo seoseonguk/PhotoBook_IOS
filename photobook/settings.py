b9e74ce6341c5f73762dc38bcefb9fbd1e2e1f3f
Seonguk, Seo <su.seo@bakecomapany.net>
2016-08-19 05:12:35 +0900

    [new] user model 생성 및 settings.py 추가

--- photobook/settings.py
+++ photobook/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
     # local app
 	'photo',
-
+    'users',
 ]
 
 MIDDLEWARE_CLASSES = [
---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+
+
+
+class User(models.Model):
+    auth_user = models.OneToOneField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.auth_user
+
+
+class Group(models.Model):
+    owner = models.ForeignKey(User)
+    user_list = models.ManyToManyField(User, related_name='user_list')
+    name = models.CharField(max_length=15)
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
