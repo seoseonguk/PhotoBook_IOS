c9a3ff90661464867ea10114af5fc068c0d20350
Seonguk, Seo <su.seo@bakecomapany.net>
2016-08-19 10:59:22 +0900

    [new] photo list api 추가

--- photo/serializers.py
+++ photo/serializers.py
@@ -7,44 +7,20 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from .models import Photo, Memory
 
 
+class PhotoUrlHyperlinkedIdentityField(serializers.HyperlinkedIdentityField):
+    pass
 
 
-# class PhotoSerializer(serailizers.HyperlinkedModelSerializer):
-#     url =
+class PhotoSerializer(serializers.HyperlinkedModelSerializer):
+    # url = PhotoUrlHyperlinkedIdentityField('photo_list_api')
 
-
-
-# class PhotoSerializer(serializers.HyperlinkedModelSerializer):
-#     memory_title = serializers.CharField(source='memory.title', read_only=True)
-#     class Meta:
-#         model = Photo
-#         fields = [
-#             'image',
-#             'memory_title',
-#             'taken_at',
-#             'created_at'
-#         ]
-
-
-# class PhotoViewSet(viewsets.ModelViewSet):
-#     queryset = Photo.objects.all()
-#     serializer_class = PhotoSerializer
-
-
-# class MemorySerailizer(serializers.HyperlinkedModelSerializer):
-#     photo_set = PhotoSerializer(many=True, read_only=True)
-#     class Meta:
-#         model = Memory
-#         fields = [
-#             'title',
-#             'image',
-#             'taken_at',
-#             'created_at',
-#             'photo_set'
-#         ]
-
-
-# class MemoryViewSet(viewsets.ModelViewSet):
-#     queryset = Memory.objects.all()
-#     serializer_class = MemorySerailizer
-
+    class Meta:
+        model = Photo
+        fields = [
+            # 'url',
+            'id',
+            'image',
+            # 'memory',
+            'taken_at',
+            'created_at',
+        ]
